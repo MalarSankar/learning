67ef2c247e53d1d672337be37cc3f482adce4d2a
MalarSankar <smalar1998@gmail.com>
2020-07-14 15:46:58 -0700

    class assignment1 commited

---
+++ class_assignment1.py
@@ -0,0 +1,24 @@
+class time:
+    def __init__(self,time1,time2):
+        self.time1=time1
+        self.time2=time2
+        self.time_list1=list(self.time1.split(" "))
+        self.time_list2=list(self.time2.split(" "))
+
+    def add(self):
+        tot_add=int(self.time_list1[0])*60+int(self.time_list1[3])+int(self.time_list2[0])*60+int(self.time_list2[3])
+        tot_add_hour=tot_add/60
+        tot_add_min=tot_add%60
+        print(int(tot_add_hour),"hours",int(tot_add_min),"mins")
+    def sub(self):
+        tot_sub=(int(self.time_list1[0])*60+int(self.time_list1[3]))-(int(self.time_list2[0])*60+int(self.time_list2[3]))
+        tot_sub_hour=tot_sub/60
+        tot_sub_min=tot_sub%60
+        print(int(tot_sub_hour),"hours",int(tot_sub_min),"mins")
+    def display_min(self):
+        tot_min=int(self.time_list1[0])*60+int(self.time_list1[3])+int(self.time_list2[0])*60+int(self.time_list2[3])
+        print(tot_min,"mins")
+t1=time("4 hours and 50 mins","2 hr and 15 mins")
+t1.add()
+t1.sub()
+t1.display_min()
