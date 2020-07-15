c255fd265fe3b65514de8daa1c63d80dd8fef289
MalarSankar <smalar1998@gmail.com>
2020-07-15 13:22:47 -0700

    updated class and object assignment

--- class_assignment1.py
+++ class_assignment1.py
@@ -11,16 +11,21 @@ class time:
         tot_add_min=tot_add%60
         print(int(tot_add_hour),"hours",int(tot_add_min),"mins")
     def sub(self):
-        tot_sub=(int(self.time_list1[0])*60+int(self.time_list1[3]))-(int(self.time_list2[0])*60+int(self.time_list2[3]))
-        tot_sub_hour=tot_sub/60
-        tot_sub_min=tot_sub%60
-        print(int(tot_sub_hour),"hours",int(tot_sub_min),"mins")
+        if(int(self.time_list1[0])>=int(self.time_list2[0])):
+            tot_sub=(int(self.time_list1[0])*60+int(self.time_list1[3]))-(int(self.time_list2[0])*60+int(self.time_list2[3]))
+            tot_sub_hour=tot_sub/60
+            tot_sub_min=tot_sub%60
+            print(int(tot_sub_hour),"hours",int(tot_sub_min),"mins")
+        else:
+            tot_sub = (int(self.time_list2[0]) * 60 + int(self.time_list2[3])) - (
+                        int(self.time_list1[0]) * 60 + int(self.time_list1[3]))
+            tot_sub_hour = tot_sub / 60
+            tot_sub_min = tot_sub % 60
+            print(int(tot_sub_hour), "hours", int(tot_sub_min), "mins")
     def display_min(self):
         tot_min=int(self.time_list1[0])*60+int(self.time_list1[3])+int(self.time_list2[0])*60+int(self.time_list2[3])
         print(tot_min,"mins")
-t1=time(input("enter time1 in the format(e.g 4 hour and 6 min)"),input("enter time2 in the format(e.g 4 hour and 6 min)"))
+t1=time(input("enter time1 in the format (e.g 4 hours and 7 min)"),input("enter time2 in the format(e.g 4 hours and 7 min)"))
 t1.add()
 t1.sub()
 t1.display_min()
-
-
