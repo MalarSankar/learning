d4f75b78044f9c0a05de8f665979270a7652354b
MalarSankar <smalar1998@gmail.com>
2020-07-16 12:55:14 -0700

    update the planet class

--- planet_class_assignment.py
+++ planet_class_assignment.py
@@ -15,24 +15,10 @@ class planet:
            self.len_of_years=self.len_of_years*self.earth_year
            print("The no of days in",self.planet_name,"is",self.len_of_years)
 
-def largest_planet(m,v,e,m1,j,s,u,n):
-    max_val=max(m.diameter,v.diameter,e.diameter,m1.diameter,j.diameter,s.diameter,u.diameter,n.diameter)
-    if(max_val==m.diameter):
-        print("the largest planet is",m.planet_name)
-    elif(max_val==v.diameter):
-        print("the largest planet is", v.planet_name)
-    elif(max_val==e.diameter):
-        print("the largest planet is", e.planet_name)
-    elif(max_val==m1.diameter):
-        print("the largest planet is", m1.planet_name)
-    elif(max_val==j.diameter):
-        print("the largest planet is", j.planet_name)
-    elif(max_val==s.diameter):
-        print("the largest planet is", s.planet_name)
-    elif(max_val==u.diameter):
-        print("the largest planet is", u.planet_name)
-    else:
-        print("the largest planet is", n.planet_name)
+def largest_planet(*planets):
+    for planet in planets:
+        if(planet.diameter==max_size):
+            print("The largest planet is",planet.planet_name)
 mercury=planet("Mercury",4879,0,88)
 venus=planet("Venus",12100,0,225)
 earth=planet("Earth",12755,1,365)
@@ -41,6 +27,8 @@ jupiter=planet("Jupiter",142800,79,12)
 saturn=planet("Saturn",120537,82,29.5)
 uranus=planet("Uranus",51819,27,84)
 neptune=planet("Neptune",49529,14,165)
+max_size=max(mercury.diameter,venus.diameter,earth.diameter,mars.diameter,jupiter.diameter,saturn.diameter,uranus.diameter,neptune.diameter)
+
 neptune.radius_of_planet()
 jupiter.num_of_days()
 largest_planet(mercury,venus,earth,mars,jupiter,saturn,uranus,neptune)
